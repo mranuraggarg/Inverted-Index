b0429d3a53097bd78e02bca4b20abb35be9ee789
mranuraggarg <66897782+mranuraggarg@users.noreply.github.com>
2020-12-29 11:00:29 +0400

    Update inverted_index_final.py

--- inverted_index_final.py
+++ inverted_index_final.py
@@ -1,15 +1,13 @@
 import json
 import argparse
-import sys
 
 
 def load_document(filepath):
     wiki_db = {}
     with open(filepath, encoding="utf8", mode="r") as f:
         for line in f.readlines():
-            (article_id, article_name_content) = line.split(maxsplit=1)
-            article_name_content = article_name_content.lstrip().rstrip()
-            wiki_db[int(article_id)] = article_name_content
+            article_id, article_name_content = line.split(maxsplit=1)
+            wiki_db[int(article_id)] = article_name_content.strip()
     return wiki_db
 
 
@@ -17,60 +15,56 @@ def build_inverted_index(articles):
     inverted_db = {}
     for key, value in articles.items():
         for word in value.split():
-            if inverted_db.get(word) is None and word.strip():
-                inverted_db[word] = {key}
-            elif word.strip():
-                inverted_db[word].add(key)
+            inverted_db.setdefault(word, set()).add(key)
     return InvertedIndex(inverted_db)
 
 
 def build(args_obj):
+    """
+
+    :type args_obj: file
+    """
     build_inverted_index(load_document(args_obj.build_infile)).dump(args_obj.build_outfile)
     return None
 
 
 def print_query(args_obj):
-    obj = InvertedIndex.load(args_obj.query_infile)
+    """
+
+    :type args_obj: file
+    """
+    inverted_obj = InvertedIndex.load(args_obj.query_infile)
     with open(args_obj.query_file, 'r') as f:
         for line in f:
-            article_ids = sorted(obj.query(line))
-            print(*article_ids, sep=',', file=sys.stdout)    # Sending output to screen
+            article_ids = sorted(inverted_obj.query(line))
+            print(*article_ids, sep=',')  # Sending output to screen
     return None
 
 
 class InvertedIndex:
-    def __init__(self, word_to_docs_mapping):
+    def __init__(self, word_to_docs_mapping) -> dict:
         self.db = word_to_docs_mapping
 
     def query(self, words):
-        article_ids = set()
-        if isinstance(words, str):
-            words = words.split()
-        itr = None
-        for itr, word in enumerate(words):
-            if not (self.db.get(word) is None):
-                article_ids |= self.db[word]
-                break
-        for word in words[itr + 1:]:
-            if not (self.db.get(word) is None):
-                article_ids &= self.db[word]
-            else:
-                article_ids &= set()
-        return article_ids
+        result = self.db.get(words[0], set()).copy()
+        for word in words:
+            result &= self.db.get(word, set())
+        return result
 
     def dump(self, filepath):
-        database = {}
+        """
+
+        :rtype: None
+        """
+        database = {key: list(value) for key, value in self.db.items()}
         with open(filepath, mode="w") as f:
-            for key, value in self.db.items():
-                database[key] = list(value)
-            json.dump(database, f)              # Writing json file to disk
+            json.dump(database, f)  # Writing json file to disk
         return None
 
     @classmethod
     def load(cls, filepath):
         with open(filepath, mode="r") as f:
-            line = f.readline()
-            inverted_db = json.loads(line)
+            inverted_db = json.loads(f)
             for key, value in inverted_db.items():
                 inverted_db[key] = set(value)
         return cls(inverted_db)
